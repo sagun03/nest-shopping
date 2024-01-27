0c65414fc0e704000e6c0028dbab6ad532c03a24
sagun03 <sagunsaluja13@gmail.com>
2024-01-27 14:08:16 -0500

    added validation pipes

--- src/users/controllers/users/users.controller.ts
+++ src/users/controllers/users/users.controller.ts
@@ -7,14 +7,22 @@ import {
   Body,
   Param,
   Query,
+  UsePipes,
+  ValidationPipe,
+  ParseIntPipe,
+  ParseBoolPipe
 } from '@nestjs/common';
 import { Request, Response } from 'express';
 import { CreateUserDto } from 'src/users/dtos/CreateUser.dto';
+
 @Controller('users')
 export class UsersController {
   @Get()
-  getUsers(@Query('sortBy') sortBy: string) {
-    console.log(sortBy);
+  getUsers(
+    @Query('sortBy') sortBy: string,
+    @Query('sortDesc', ParseBoolPipe) sortDesc: boolean,
+  ) {
+    console.log(sortBy, sortDesc);
     return {
       userName: 'sagun',
     };
@@ -41,15 +49,21 @@ export class UsersController {
 
   // using Body anomatore
   @Post('create')
+  @UsePipes(new ValidationPipe())
   createUsers(@Body() userData: CreateUserDto) {
     // we can use body decorator to type annoate my request
     console.log(userData);
     return {};
     // response.send('User Created');
   }
+  @Get(':id')
+  getUserById(@Param('id', ParseIntPipe) id: number) {
+    console.log('id>>', id);
+    return { id };
+  }
   // nested as well
   @Get(':id/:postId')
-  getUserById(@Param('id') id: string, @Param('postId') postId: string) {
+  getUserByIdAndPost(@Param('id') id: string, @Param('postId') postId: string) {
     console.log('id', id, postId);
     return id;
   }
